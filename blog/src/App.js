69db623c61d44799dcea70163135f00e65db7d16
seungbeen <seungbin803@gmail.com>
2023-04-13 01:16:11 +0900

    state로 title 3개 만들기

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,16 +10,29 @@ function App() {
   // 1. import { useState }
   // 2. useState(보관할 자료)
   // 3. let[작명, 작명] => [state에 보관했던 자료, state 변경도와주는 함수]
-  let [title, b] = useState('남자 코트 추천')
+  
+  // 숙제 : 제목들 state로 만들기
+  // array 자료형
+  let [title, b] = useState(['남자코트 추천', '강남 우동맛집', '리액트독학']);
+  // let [title2, c] = useState('강남 우동맛집');
+  // let [title3, d] = useState('리액트독학');
   
   // return() 안에는 병렬로 태그 2개 이상 기입금지
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4>블로그</h4>
+        <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>{ title }</h4>
+        <h4>{title[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
