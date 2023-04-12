f8d01e2644c08f857b7a7df8901526c19faef023
seungbeen <seungbin803@gmail.com>
2023-04-13 02:13:23 +0900

    버튼누르면 첫 글이 '여자코트 추천'으로 바뀌는 기능만들기, array/object

--- blog/src/App.js
+++ blog/src/App.js
@@ -24,9 +24,23 @@ function App() {
       </div>
 
       {/* 숙제 : 버튼누르면 첫 글이 '여자코트 추천'으로 바뀌는 기능만들기 */}
+      {/* state가 array/object면 독립적 카피본을 만들어서 수정해야 함 */}
       <button onClick={() => {
-        titleChange(['여자코트 추천', '강남 우동맛집', '리액트독학']);
-      }}>글 수정</button>
+        // arry/object를 다룰 때 원본은 보존하는게 좋음
+        // state변경함수 특징 -> 기존state == 신규state의 경우 변경안해줌
+        // array/object 특징 -> array/object 담은 변수엔 화살표만 저장됨
+
+        // array를 수정했지 변수에 있던 화살표는 수정이 안됨
+        // 변수1 & 변수2 화살표가 같으면 변수1 == 변수2 비교해도 true 나옴
+        // [...변수] : 화살표를 새로 바꿔준다는 의미
+        // ... : 괄호 벗기기
+        let copy = [...title];
+        copy[0] = '여자코트 추천';
+        titleChange(copy);
+      }}>글수정</button>
+
+      {/* 숙제 : 버튼 누르면 글제목 가나다순 정렬 기능 만들기 */}
+      <button >가나다순정렬</button>
 
       <div className='list'>
         {/* state 변경하는 법-> 등호로 변경금지 */}
