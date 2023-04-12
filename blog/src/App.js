36e5cccb0b12194ff7d5de23042bbcee0e138ffb
seungbeen <seungbin803@gmail.com>
2023-04-13 00:34:38 +0900

    JSX 문법 1. class 넣을 땐 className 2. 변수를 넣을 때 {중괄호} => 데이터바인딩은{중괄호} 3. style 넣을 땐 style={}

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+// css파일을 쓰려면 상단에서 import 'css파일경로'
 import './App.css';
 
 function App() {
+
+  let post = "강남 우동 맛집";
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>블로그</h4>
+      </div>
+      <h4 style={ {color : 'lightcoral'} }>{ post }</h4>
     </div>
   );
 }
